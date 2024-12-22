d103eaa3ba097995caf560d530374806a1a116d7
Subhra Prakash Jena <jsubhra94@gmail.com>
2024-12-22 17:30:26 +0530

    added Dockerfile

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 
 
@@ -13,7 +12,3 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
 	return {"message": f"Hello {name}"}
-
-
-if __name__ == "__main__":
-	uvicorn.run(app, host="0.0.0.0", port=8080)
